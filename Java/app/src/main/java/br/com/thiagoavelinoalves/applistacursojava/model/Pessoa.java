462f4454674134522c151c3df5d8cdc1fbc72dcf
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2024-10-01 00:36:04 -0300

    Estruturando o projeto APP lista

---
+++ Java/app/src/main/java/br/com/thiagoavelinoalves/applistacursojava/model/Pessoa.java
@@ -0,0 +1,34 @@
+package br.com.thiagoavelinoalves.applistacursojava.model;
+
+public class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+
+    public Pessoa(){
+
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getSobrenome(){
+        return this.sobrenome;
+    }
+    public void setSobrenome(String sobrenome){
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone(){
+        return this.telefone = this.telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+}
