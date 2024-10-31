742429d591953959ecdaf5d3ad0781494e248dfc
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2024-10-30 22:58:44 -0300

    Implementando o banco de dados

--- Java/AppGasEtanol/app/src/main/java/br/com/thiagoavelinoalves/appgasetanol/controller/CombustivelController.java
+++ Java/AppGasEtanol/app/src/main/java/br/com/thiagoavelinoalves/appgasetanol/controller/CombustivelController.java
@@ -1,27 +1,40 @@
 package br.com.thiagoavelinoalves.appgasetanol.controller;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 
+import br.com.thiagoavelinoalves.appgasetanol.DataBase.GasEtanolDB;
 import br.com.thiagoavelinoalves.appgasetanol.model.Combustivel;
 import br.com.thiagoavelinoalves.appgasetanol.view.GasEtanolActivity;
 
-public class CombustivelController {
+public class CombustivelController extends GasEtanolDB {
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor combustivelRecomendado;
     public static final String NOME_PREFERENCES = "pref_combustivel";
 
     public CombustivelController(GasEtanolActivity gasEtanolActivity){
+        super(gasEtanolActivity);
         sharedPreferences = gasEtanolActivity.getSharedPreferences(NOME_PREFERENCES,0);
         combustivelRecomendado = sharedPreferences.edit();
     }
 
     public void salvar(Combustivel combustivel){
+
+        ContentValues contentValues = new ContentValues();
+
         combustivelRecomendado.putString("NomeCombustivel", combustivel.getNomeCombustivel());
         combustivelRecomendado.putString("PrecoCombustivel", String.valueOf(combustivel.getPrecoCombustivel()));
         combustivelRecomendado.putString("Recomendacao",combustivel.getRecomendacao());
-
         combustivelRecomendado.apply();
+
+        contentValues.put("PRECO", combustivel.getPrecoCombustivel());
+        contentValues.put("NOME", combustivel.getNomeCombustivel());
+        contentValues.put("RECOMENDACAO", combustivel.getRecomendacao());
+
+
+        salvarDados("COMBUSTIVEL", contentValues);
+
     }
 
     public void limpar(){
