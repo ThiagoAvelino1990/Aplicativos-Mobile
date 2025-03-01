0b6849951f497dcc64edd794083e32e0a1fbdd38
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-03-01 14:27:42 -0300

    Implementando o plugin realm

--- Java/AppClientes/app/build.gradle.kts
+++ Java/AppClientes/app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -37,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.realm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/MainActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity
     MenuItem nav_vermelho;
     MenuItem nav_azul;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
