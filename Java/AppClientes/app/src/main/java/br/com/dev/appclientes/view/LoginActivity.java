1a48e0cc405dd3c7a01d98670a50f78615d30614
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-03-09 20:15:37 -0300

    criacao da tela de login

---
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/LoginActivity.java
@@ -0,0 +1,35 @@
+package br.com.dev.appclientes.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.dev.appclientes.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
