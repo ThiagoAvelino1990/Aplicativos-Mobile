da12d88802e0272ca153da75c257de27c1c8470b
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-03-13 22:34:10 -0300

    implementação de validações na tela de cadastro

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/CadastroActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/CadastroActivity.java
@@ -1,6 +1,7 @@
 package br.com.dev.appclientes.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.dev.appclientes.R;
+import br.com.dev.appclientes.api.AppUtil;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -20,6 +23,8 @@ public class CadastroActivity extends AppCompatActivity {
     TextView txtViewCadastro, txtCadastroNovoUsuario;
     EditText editNomeCompleto, editCadastroEmail, editSenha, editConfirmarSenha;
     Button btnConfirmarCadastro, btnVoltarCadastro;
+
+    SharedPreferences preferencesCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +37,65 @@ public class CadastroActivity extends AppCompatActivity {
         btnVoltarCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
                 startActivity(intent);
+
             }
         });
+
+        btnConfirmarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+
+                if(validarDados()){
+                    setDataPreferences();
+                    intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    private boolean validarDados() {
+
+        boolean isDadosOK = true;
+
+        if(editNomeCompleto.getText().toString().isEmpty() || editCadastroEmail.getText().toString().isEmpty()){
+            isDadosOK = false;
+            editNomeCompleto.requestFocus();
+            editNomeCompleto.setError("*");
+            editCadastroEmail.requestFocus();
+            editCadastroEmail.setError("*");
+
+        }
+
+        if((editSenha.getText().toString().isEmpty()) || (!editSenha.getText().toString().equals(editConfirmarSenha.getText().toString()))){
+            editSenha.requestFocus();
+            editSenha.setError("*");
+            editConfirmarSenha.requestFocus();
+            editConfirmarSenha.setError("*");
+            isDadosOK = false;
+        }
+
+        if(!isDadosOK){
+            Toast.makeText(CadastroActivity.this,"* Favor verificar os dados",Toast.LENGTH_LONG).show();
+        }
+
+    return isDadosOK;
+
+    }
+
+    private void setDataPreferences() {
+
+        preferencesCadastro = getSharedPreferences(AppUtil.PREF_APP, MODE_PRIVATE);
+        SharedPreferences.Editor data = preferencesCadastro.edit();
+
+        data.putString("nome",editNomeCompleto.getText().toString());
+        data.putString("email",editCadastroEmail.getText().toString());
+        data.putString("senha",editSenha.getText().toString());
 
     }
 
