1ee1151b5d97ea3c03da1664c6cc2d1031933c94
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-03-28 00:10:24 -0300

    Inclusão da função para validar CNPJ

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/api/AppUtil.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/api/AppUtil.java
@@ -12,6 +12,20 @@ public class AppUtil {
     public static final String VERSION = "v1.0.0";
     public static final String PREF_APP = "app_clientes_pref";
 
+    public static final String TERMOS_DE_USO = "Última atualização: [data]\n" +
+            "\n" +
+            "Ao usar o aplicativo [Nome do Aplicativo], você concorda com os seguintes termos:\n" +
+            "\n" +
+            "Aceitação: Ao acessar o app, você aceita estes termos. Se não concordar, não use.\n" +
+            "Alterações: Podemos atualizar os termos a qualquer momento. Verifique periodicamente.\n" +
+            "Uso: Use o app de maneira legal. Não modifique ou distribua seu conteúdo sem permissão.\n" +
+            "Conta: Você é responsável por sua conta. Notifique-nos sobre uso não autorizado.\n" +
+            "Privacidade: O uso de dados está descrito na [Política de Privacidade].\n" +
+            "Responsabilidade: Não nos responsabilizamos por danos ou falhas, conforme permitido por lei.\n" +
+            "Rescisão: Podemos suspender sua conta por violação. Você pode encerrar a conta a qualquer momento.\n" +
+            "Legislação: Este termo é regido pelas leis de [país/região].\n" +
+            "Contato: Dúvidas? Entre em contato em [e-mail de contato].";
+
     /**
      *
      * @return Retorna a data no formato "dd/MM/yyyy 00:00:00"
@@ -29,19 +43,65 @@ public class AppUtil {
 
     }
 
-    public static final String TERMOS_DE_USO = "Última atualização: [data]\n" +
-            "\n" +
-            "Ao usar o aplicativo [Nome do Aplicativo], você concorda com os seguintes termos:\n" +
-            "\n" +
-            "Aceitação: Ao acessar o app, você aceita estes termos. Se não concordar, não use.\n" +
-            "Alterações: Podemos atualizar os termos a qualquer momento. Verifique periodicamente.\n" +
-            "Uso: Use o app de maneira legal. Não modifique ou distribua seu conteúdo sem permissão.\n" +
-            "Conta: Você é responsável por sua conta. Notifique-nos sobre uso não autorizado.\n" +
-            "Privacidade: O uso de dados está descrito na [Política de Privacidade].\n" +
-            "Responsabilidade: Não nos responsabilizamos por danos ou falhas, conforme permitido por lei.\n" +
-            "Rescisão: Podemos suspender sua conta por violação. Você pode encerrar a conta a qualquer momento.\n" +
-            "Legislação: Este termo é regido pelas leis de [país/região].\n" +
-            "Contato: Dúvidas? Entre em contato em [e-mail de contato].";
+    public static boolean validaCnpjCpf(String cpfCnpj){
+
+        cpfCnpj = "14572457000185";
+
+        int peso = 2;
+        int soma = 0;
+        int somaTotal = 0;
+        int digVerifUm, digVerifDois;
+        String cnpj = cpfCnpj;
+        int tam = cpfCnpj.length() - 2;
+
+        //Cálculo para validação do CNPJ
+        // Calcular o Dígito verificador 1
+        for (int i = tam; i > 0; i--){
+            soma = ((int)cnpj.charAt(i) * peso);
+            peso ++;
+
+            if(peso > 9){
+                peso = 2;
+            }
+            somaTotal = somaTotal + soma;
+        }
+
+        if(soma % 11 == 0 || soma % 11 == 1){
+            digVerifUm = 0;
+        }else{
+            digVerifUm = 11 - (soma % 11);
+        }
+
+        soma = 0;
+        peso = 0;
+        cnpj = digVerifUm+cnpj;
+        tam  = cpfCnpj.length() - 1;
+
+        for(int i = tam; i > 0; i--){
+            soma = ((int)cnpj.charAt(i) * peso);
+            peso ++;
+
+            if(peso > 9){
+                peso = 2;
+            }
+            somaTotal = somaTotal + soma;
+        }
+
+        if(soma % 11 == 0 || soma % 11 == 1){
+            digVerifDois = 0;
+        }else{
+            digVerifDois = 11 - (soma % 11);
+        }
+
+        if((digVerifUm == cpfCnpj.charAt(12)) && (digVerifDois == cpfCnpj.charAt(13))){
+            return true;
+        }
+
+        return false;
+
+    }
+
+
 
 
 
