5f1ff2133c592921ac389252f199e08affdbff85
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-04-13 10:12:08 -0300

    Alteração na tela para recuperar senha.

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/RecuperarSenhaActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/RecuperarSenhaActivity.java
@@ -3,6 +3,7 @@ package br.com.dev.appclientes.view;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,8 +24,11 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
 
     EditText editRecuperarSenha;
     Button btnRecuperarSenha, btnVoltar;
+
     Usuario usuario;
     UsuarioController usuarioController;
+
+    SmsManager smsManager;
 
 
     @Override
@@ -41,11 +45,15 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
                 Intent intent;
 
                 if(validaTelefone()){
+                    if(enviarSenha()){
+                        Toast.makeText(RecuperarSenhaActivity.this,"Dados enviados para o telefone informado",Toast.LENGTH_LONG).show();
 
-                    Toast.makeText(RecuperarSenhaActivity.this,"Dados enviados para o telefone informado",Toast.LENGTH_LONG).show();
+                        intent = new Intent(RecuperarSenhaActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(RecuperarSenhaActivity.this,"[ERRO] - Falha ao enviar a senha",Toast.LENGTH_LONG).show();
+                    }
 
-                    intent = new Intent(RecuperarSenhaActivity.this,LoginActivity.class);
-                    startActivity(intent);
                 }else{
                     Toast.makeText(RecuperarSenhaActivity.this,"Verifique se o telefone informado está correto e tente novamente",Toast.LENGTH_LONG).show();
                 }
@@ -63,6 +71,17 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private boolean enviarSenha() {
+
+        try{
+            smsManager.sendTextMessage(editRecuperarSenha.getText().toString(), null, "Nova senha: ",
+                    null, null);
+         return true;
+        }catch(Exception err){
+        return false;
+        }
     }
 
     private boolean validaTelefone() {
