3cb78b979d07939892f8771e30be037e47dc3474
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-04-14 22:52:31 -0300

    adicionando a opção de sair do aplicativo

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/MainActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/MainActivity.java
@@ -1,6 +1,6 @@
 package br.com.dev.appclientes.view;
 
-import android.content.ClipData;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +22,8 @@ import androidx.fragment.app.FragmentManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
 
 import br.com.dev.appclientes.R;
 import br.com.dev.appclientes.api.AppUtil;
@@ -169,7 +172,24 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sair) {
+            FancyAlertDialog.Builder
+                    .with(MainActivity.this)
+                    .setTitle("Sair do Aplicativo")
+                    .setBackgroundColorRes(R.color.splash_bgr)
+                    .setMessage("Deseja Sair do aplicativo?")
+                    .setNegativeBtnText("Não")
+                    .setPositiveBtnBackgroundRes(R.color.splash_bgr)
+                    .setPositiveBtnText("Sim")
+                    .setNegativeBtnBackgroundRes(R.color.splash_bgr)
+                    .setAnimation(Animation.POP)
+                    .isCancellable(true)
+                    .setIcon(R.mipmap.ic_launcher_round, View.VISIBLE)
+                    .onPositiveClicked(dialog -> finish())
+                    .onNegativeClicked(dialog -> closeContextMenu())
+                    .build()
+                    .show();
+            finish();
             return true;
         }
 
