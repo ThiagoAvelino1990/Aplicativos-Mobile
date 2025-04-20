0c3353315c909c4f70f8db260fb218bb269f30ec
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-04-20 12:15:10 -0300

    adicionando a tela para trocar a senha

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/LoginActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/LoginActivity.java
@@ -3,14 +3,21 @@ package br.com.dev.appclientes.view;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.dev.appclientes.R;
 import br.com.dev.appclientes.api.AppUtil;
@@ -166,10 +173,63 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * TODO :Criar método mudar a senha
      */
-    public void validaTrocarSenha(){
+    public void validarTrocaDeSenha(){
+        sharedPreferences = getSharedPreferences(AppUtil.PREF_APP, MODE_PRIVATE);
 
+        List<Usuario> usuarioLista = new ArrayList<>();
 
+        usuarioLista = usuarioController.readObjectById(Integer.parseInt(sharedPreferences.getString("idUsuario",String.valueOf(-1))));
+
+        for(Usuario usuario : usuarioLista){
+            if(usuario.getAtualizaSenha().equals("S")){
+
+            }
+        }
     }
+
+    public void atualizarSenha(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setTitle("Atualizar Senha");
+
+        //Criando o layout
+        LinearLayout linearLayout = new LinearLayout(LoginActivity.this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(50, 40,50,10);
+
+        //Campos para senha
+        final EditText novaSenha = new EditText(LoginActivity.this);
+        novaSenha.setHint("Nova senha");
+        novaSenha.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        linearLayout.addView(novaSenha);
+
+        //Confirmação da senha
+        final EditText confirmarSenha = new EditText(LoginActivity.this);
+        confirmarSenha.setHint("Confirmar senha");
+        confirmarSenha.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        linearLayout.addView(confirmarSenha);
+
+        builder.setView(linearLayout);
+
+        //Botão Confirmar
+        builder.setPositiveButton("OK", (dialog, which) ->{
+           String senhaNova = novaSenha.getText().toString();
+           String senhaConfirmar = confirmarSenha.getText().toString();
+
+            /**
+             * TODO: Criar lógica para salvar a senha
+             */
+            if(senhaNova.isEmpty() || (!senhaNova.equals(senhaConfirmar))){
+               Toast.makeText(LoginActivity.this,"As senhas não ocnferem",Toast.LENGTH_LONG).show();
+           }else{
+               Toast.makeText(LoginActivity.this,"Senha atualizada com sucesso",Toast.LENGTH_LONG).show();
+           }
+
+           AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        });
+    }
+
 
 
 }
