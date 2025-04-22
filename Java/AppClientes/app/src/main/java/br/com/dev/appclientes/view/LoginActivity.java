f1ca01bb2d7095fbb2ab9b0c3c526fc8201d021b
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-04-21 21:32:26 -0300

    TODO para futuras alterações

--- Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/LoginActivity.java
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/view/LoginActivity.java
@@ -58,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent;
-
+                Log.i(AppUtil.TAG,"1- Entrou aqui");
                 validarTrocaDeSenha();
 
                 if(validarDadosFormulario()) {
@@ -182,6 +182,7 @@ public class LoginActivity extends AppCompatActivity {
 
         for(Usuario usuario : usuarioLista){
             if(usuario.getAtualizaSenha().equals("S")){
+                Log.i(AppUtil.TAG,"2- Entrou aqui");
                 atualizarSenha();
             }
         }
@@ -194,9 +195,12 @@ public class LoginActivity extends AppCompatActivity {
     public void atualizarSenha(){
         sharedPreferences = getSharedPreferences(AppUtil.PREF_APP, MODE_PRIVATE);
         SharedPreferences.Editor dadosSalvos = sharedPreferences.edit();
+        Log.i(AppUtil.TAG,"3- Entrou aqui");
 
         AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
         builder.setTitle("Atualizar Senha");
+        Log.i(AppUtil.TAG,"4- Entrou aqui");
+
 
         //Criando o layout
         LinearLayout linearLayout = new LinearLayout(LoginActivity.this);
@@ -217,41 +221,41 @@ public class LoginActivity extends AppCompatActivity {
 
         builder.setView(linearLayout);
 
-        //Botão Confirmar
-        builder.setPositiveButton("OK", (dialog, which) ->{
-           String senhaNova = novaSenha.getText().toString();
-           String senhaConfirmar = confirmarSenha.getText().toString();
-
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            String senhaNova = novaSenha.getText().toString();
+            String senhaConfirmar = confirmarSenha.getText().toString();
+            Log.i(AppUtil.TAG,"7- Entrou aqui");
 
             if(senhaNova.isEmpty() || (!senhaNova.equals(senhaConfirmar))){
-               Toast.makeText(LoginActivity.this,"As senhas não ocnferem",Toast.LENGTH_LONG).show();
-           }else{
+                Toast.makeText(LoginActivity.this,"As senhas não conferem",Toast.LENGTH_LONG).show();
+            }else{
                 try{
                     dadosSalvos.putString("senha",AppUtil.criptografarPass(senhaConfirmar));
+                    dadosSalvos.apply();
+
                     editLoginSenha.setText(sharedPreferences.getString("senha",null));
 
 
                     usuario.setId(Integer.parseInt(sharedPreferences.getString("idUsuario",String.valueOf(-1))));
                     usuario.setEmail(sharedPreferences.getString("email",null));
                     usuario.setSenha(sharedPreferences.getString("senha",null));
-
-
+                    //usuario.setAtualizaSenha("N");
 
                     usuarioController.insertObject(usuario);
 
-
-
                     Toast.makeText(LoginActivity.this,"Senha atualizada com sucesso",Toast.LENGTH_LONG).show();
+
                 }catch(Exception err){
                     Log.e(AppUtil.TAG,"Erro ao atualizar nova senha "+err.getMessage());
                 }
 
-           }
-
-           AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-
+            }
         });
+
+        AlertDialog alertDialog = builder.create();
+        Log.i(AppUtil.TAG,"5- Entrou aqui");
+
+        alertDialog.show();
     }
 
 
