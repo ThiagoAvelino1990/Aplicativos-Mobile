269af98235373cb292f03ec9e76da882bcba73de
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-05-18 23:36:14 -0300

    criacao da classe adapter

---
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/adapter/ClienteAdapter.java
@@ -0,0 +1,28 @@
+package br.com.dev.appclientes.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import br.com.dev.appclientes.model.Cliente;
+
+public class ClienteAdapter extends ArrayAdapter<Cliente> implements View.OnClickListener{
+
+    Context contexto;
+
+    private static class ViewHolder{
+
+    }
+
+    public ClienteAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
