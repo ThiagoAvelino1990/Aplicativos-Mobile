d4f1a60f0577c11bc231cdd4f4dc5d9ac6d7f31a
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-06-29 18:00:57 -0300

    Criando a classe AppUtilWebService

---
+++ Java/AppClientes/app/src/main/java/br/com/dev/appclientes/api/AppUtilWebService.java
@@ -0,0 +1,16 @@
+package br.com.dev.appclientes.api;
+
+public class AppUtilWebService {
+    //Url do servidor apache - PHP
+    //192.168.0.21 -- > ipconfig
+    public static final String URL_WEB_SERVICE = "http://localhost/meusprojetos/appclientes/index.php";
+
+    //TIMEOUT 5 segundos
+    public static final int CONNECTION_TIMEOUT = 5000;
+
+    //15 segundos tempo de resposta
+    public static final int READ_TIMEOUT = 15000;
+
+
+
+}
