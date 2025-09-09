5106d401e74088e4c59389c50de478372c41a341
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-09-09 00:36:59 -0300

    Ajustes no arquivo de inserção do usuario

--- webService/appClientes/insertUsuario.php
+++ webService/appClientes/insertUsuario.php
@@ -1,24 +1,24 @@
- <?php
+<?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
-    $token = $_GET['token'];
+    $token 				= $_POST['token'] ?? null;    
+    $id_tipo_pessoa 	= $_POST['ID_TIPO_PESSOA'] ?? null;
+	$nome 				= $_POST['NOME'] ?? null;
+	$cpfcnpj 			= $_POST['CPF_CNPJ'] ?? null;
+	$logradouro 		= $_POST['LOGRADOURO'] ?? null;
+	$complemento 		= $_POST['COMPLEMENTO'] ?? null;
+	$email 				= $_POST['EMAIL'] ?? null;
+	$senha 				= $_POST['SENHA'] ?? null;
+	$telefone 			= $_POST['TELEFONE'] ?? null;
+	$lembrar_senha 		= $_POST['LEMBRAR_SENHA'] ?? 0;
+	$atualizar_senha 	= $_POST['ATUALIZAR_SENHA'] ?? 0;
+	$data_de_inclusao 	= $_POST['DATA_DE_INCLUSAO'] ?? null;
+	$data_de_alteracao 	= $_POST['DATA_DE_ALTERACAO'] ?? null; 
     
-    $id_tipo_pessoa = $_GET['id_tipo_pessoa'];
-	$nome = $_GET['nome'];
-	$cpfcnpj = $_GET['cpfcnpj'];
-	$logradouro = $_GET['logradouro'];
-	$complemento = $_GET['complemento'];
-	$email = $_GET['email'];
-	$senha = $_GET['senha'];	
-	$telefone = $_GET['telefone'];
-	$lembrar_senha = $_GET['lembrar_senha'];
-	$atualizar_senha = $_GET['atualizar_senha'];
-	$data_de_inclusao = $_GET['data_de_inclusao'];
-	$data_de_alteracao = $_GET['data_de_alteracao']; 
-    
-    if ($token == "xpto" {
+    if ($token == "xpto") {
         include_once "dbConnection.php";
-        $sql = "INSERT INTO usuario (idtipopessoa, nome, cpf_cnpj, logradouro, complemento, email, senha, telefone, lembrar_senha, atualizar_senha, data_de_inclusao, data_de_alteracao)									 
+		
+        $sql = "INSERT INTO USUARIO (ID_TIPO_PESSOA, NOME, CPF_CNPJ, LOGRADOURO, COMPLEMENTO, EMAIL, SENHA, TELEFONE, LEMBRAR_SENHA, ATUALIZAR_SENHA, DATA_DE_INCLUSAO, DATA_DE_ALTERACAO)									 
 				VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 				
         $statement = $pdo->prepare($sql);
@@ -37,16 +37,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$statement->bindParam(12, $data_de_alteracao);	
 		
 
-        $statement->execute();
-       // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        //echo "<pre>".print_r($results)."</pre"; 
-        //echo$json = json_encode($results);
-        echo "Registro incluso com sucesso";
+        if($statement->execute()){
+			echo json_encode(["Status"=>"Sucesso","Mensagem"=>"Registro incluso com sucesso"]);
+		}else{
+			$error = $statement->errorInfo();			
+			echo json_encode(["Status"=>"Erro", "Mensagem"=>"Erro ao realizar o INSERT ".$error[2]]);			
+		}      
+        
     
-    }else{
-        echo "Não autorizado.";
+    }else{        
+		echo json_encode(["Status"=>"Erro", "Mensagem"=>"Não autorizado"]);			
     }
-} else {
-    echo "Acesso Negado.";
+} else {    
+	echo json_encode(["Status"=>"Erro", "Mensagem"=>"Acesso Negado"]);			
 }
 ?>
